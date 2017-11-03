d6f04a106a8069e6413e0732cab23b8ca65ae41a
Wes Duenow <wjduenow@raybeam.com>
2017-11-03 14:17:57 -0700

    Fixed Auth Bug on ES_DSL

--- product_api/urls.py
+++ product_api/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^basic_search$', views.basic_search, name='basic_search'),
+    url(r'^facets$', views.facets, name='facets'),
 ]
--- product_api/views.py
+++ product_api/views.py
@@ -9,34 +9,89 @@ from rest_framework_xml.renderers import XMLRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import serializers
-from elasticsearch import Elasticsearch
+
 from elasticsearch_dsl import Search, FacetedSearch, TermsFacet, DateHistogramFacet
+from elasticsearch_dsl.aggs import Terms, DateHistogram
 import collections
 import json
+from elasticsearch import TransportError
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
 
 # Create your views here.
 
 
-client = Elasticsearch()
+
+
+class FSearch(FacetedSearch):
+    #doc_types = ['Article']
+    # fields that should be searched
+    #client = Elasticsearch()
+    fields = ['product_name']
+    #using = client
+    #['short_product_description','long_product_description', 'product_name', 'color']
+    #index = "logstash-*"
+    facets = {
+        'manufacturer_name': TermsFacet(field='manufacturer_name.keyword'),
+        'color': TermsFacet(field='color.keyword')
+    }
+
+
+
+    #def search(self):
+    #    # override methods to add custom pieces
+    #    s = super().search()
+    #    return s#.filter('range', publish_from={'lte': 'now/h'})
+
+
+@api_view(['GET'])
+def facets(self):
+    bs = FSearch('shoes')
+    print bs.count()
+    #', {'publishing_frequency': date(2015, 6)})
+    response = bs.execute()
+    print bs.count()
+
+    # access hits and other attributes as usual
+    print(response.hits.total, 'hits total')
+    for hit in response:
+        print(hit.meta.score, hit.product_name)
+
+    for (manufacturer_name, count, selected) in response.facets.manufacturer_name:
+        print(manufacturer_name, ' (SELECTED):' if selected else ':', count)
+
+    return Response(response.hits) 
+
+
 
 @api_view(['GET'])
 def basic_search(self):
-	text_query = self.query_params.get('text', 'shirt')
 
-	s = Search(using=client, index="logstash-*") \
-	    .query("match_phrase", product_name=text_query)[0:10]   \
+    text_query = self.query_params.get('text', 'shirt')
 
-	results = s.execute()
-	results_dict = results.to_dict()
-	print results_dict['hits']
-	results = results_dict['hits']
+    s = Search(index="logstash-*") \
+        .query("match_phrase", product_name=text_query)[0:10]
 
-	page = 1
-	total_count = s.count()
+    s.aggs.bucket('per_tag', 'terms', field='manufacturer_name.keyword')
 
-	context = format_results(results, total_count, page, self, 'products', text_query)
+    response = s.execute()
 
-	return Response(context) 
+    for tag in response.aggregations.per_tag.buckets:
+        print(tag.key, tag.doc_count)    
+
+    results = s.execute()
+    results_dict = results.to_dict()
+    results = results_dict['hits']
+
+    page = 1
+    total_count = s.count()
+
+    context = format_results(results, total_count, page, self, 'products', text_query)
+
+    return Response(context) 
+
+
+
 
 
 def format_results(results, total_count, page, request, label, text_query):
@@ -50,3 +105,7 @@ def format_results(results, total_count, page, request, label, text_query):
     response['object'] = label
     response['data'] = results['hits']
     return response
+
+
+
+
