88f1a6e6bb8e9fae11b1b822c9401fe1597dc5a2
Wes Duenow <wjduenow@raybeam.com>
2017-11-24 10:39:23 -0800

    Added Link to Documentation on Decorators

--- shopping_tool/decorators.py
+++ shopping_tool/decorators.py
@@ -2,7 +2,8 @@ from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseRedirect, HttpResponse
 from .models import WpUsers
 
-
+# https://docs.djangoproject.com/en/1.11/topics/http/decorators/
+# https://simpleisbetterthancomplex.com/2015/12/07/working-with-django-view-decorators.html
 def check_login(function):
     def wrap(request, *args, **kwargs):
         if request.COOKIES.get('user_email', False):
