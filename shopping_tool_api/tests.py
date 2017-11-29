5c19348643e957ea056241a9f11406b8ef2e3108
Wes Duenow <wjduenow@raybeam.com>
2017-11-29 09:27:02 -0800

    shopping_tool/migrations/0001_initial.py

--- shopping_tool_api/tests.py
+++ shopping_tool_api/tests.py
@@ -6,15 +6,49 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from django.test import TestCase
 from shopping_tool.models import Look, AllumeStylingSessions, WpUsers
+from django.db import migrations, models
 
 class LookTests(APITestCase):
 
     def setUp(self):
-    	print "Starting Setup"
-    	WpUsers.objects.create(user_email= "shopper@allume.co")
-    	WpUsers.objects.create(user_email= "client@allume.co")
+
+        print("Creating WP Users")
+        migrations.CreateModel(
+                    name='WpUsers',
+                    fields=[
+                        ('id', models.AutoField(db_column='ID', primary_key=True, serialize=False)),
+                        ('user_login', models.CharField(max_length=60, unique=True)),
+                        ('user_pass', models.CharField(max_length=255)),
+                        ('user_nicename', models.CharField(blank=True, max_length=50, null=True)),
+                        ('user_email', models.CharField(max_length=100, unique=True)),
+                        ('user_url', models.CharField(blank=True, max_length=100, null=True)),
+                        ('user_registered', models.DateTimeField(blank=True, null=True)),
+                        ('user_activation_key', models.CharField(blank=True, max_length=255, null=True)),
+                        ('user_status', models.IntegerField(blank=True, null=True)),
+                        ('display_name', models.CharField(blank=True, max_length=250, null=True)),
+                        ('first_name', models.CharField(blank=True, max_length=255, null=True)),
+                        ('last_name', models.CharField(blank=True, max_length=255, null=True)),
+                        ('user_phone', models.CharField(blank=True, max_length=100, null=True, unique=True)),
+                        ('notes', models.TextField(blank=True, null=True)),
+                        ('timezone', models.CharField(blank=True, max_length=50, null=True)),
+                        ('last_modified', models.DateTimeField(blank=True, null=True)),
+                        ('last_login', models.DateTimeField(blank=True, null=True)),
+                        ('is_superuser', models.IntegerField()),
+                        ('is_staff', models.IntegerField()),
+                        ('is_active', models.IntegerField()),
+                        ('system_generated', models.TextField()),
+                    ],
+            options={
+                'db_table': 'wp_users',
+            },
+        )
+
+        print("Done Creating WP Users")
+
+        #WpUsers.objects.create(user_email= "shopper@allume.co")
+        #WpUsers.objects.create(user_email= "client@allume.co")
         AllumeStylingSessions.objects.create(name="Test Styling Session", shopper=1, client=1)
-        print "Ending Setup"
+
        
 
     def test_create_account(self):
