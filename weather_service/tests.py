64d1c91cf737e9e28c3495afca0481a858a4ebf6
Jay Salandanan <jsalandanan@raybeam.com>
2017-12-13 14:06:55 -0800

    weather update tests

--- weather_service/tests.py
+++ weather_service/tests.py
@@ -6,24 +6,6 @@ import datetime
 from django.test import TestCase
 from .models import Weather
 
-# test update weather data
-class UpdateOnStaleDataTests(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.CURRENT_YEAR = datetime.datetime.now().year
-        cls.LAST_UPDATED_YEAR = 2012
-
-
-    fixtures = ['UpdateOnStaleDataTests']
-
-    def test(self):
-        w = Weather.objects.get(city='San Jose', state='CA')
-        last_modified_year = w.last_modified.year
-        self.assertEqual(self.LAST_UPDATED_YEAR, last_modified_year)
-        w = Weather.objects.retrieve_weather_object(city='San Jose', state='CA')
-        self.assertEqual(self.CURRENT_YEAR, w.last_modified.year)
-
-
 class SingleWeatherRetrievalTests(TestCase):
     @classmethod
     def setUpTestData(cls):
@@ -165,5 +147,73 @@ class BulkWeatherRetrievalTests(TestCase):
         self.assertEqual('TX', weathers[2].state)
         self.assertEqual(6, Weather.objects.count())
 
+# test update weather data
+class UpdateOnStaleDataTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.CURRENT_YEAR = datetime.datetime.now().year
+        cls.LAST_UPDATED_YEAR_1 = 2012
+        cls.LAST_UPDATED_YEAR_2 = 2010
+        cls.LAST_UPDATED_YEAR_3 = 2008
+        cls.LAST_UPDATED_YEAR_4 = 2007
+        cls.LAST_UPDATED_YEAR_5 = 2015
+        cls.FRESH_YEAR = datetime.datetime.now().year - 1
+
+        # cls.LAST_UPDATED_MONTH = 
+
+    fixtures = ['UpdateOnStaleDataTests']
+
+    def test_update_stale_data_single(self):
+        w = Weather.objects.get(city='San Jose', state='CA')
+        last_modified_year = w.last_modified.year
+        self.assertEqual(self.LAST_UPDATED_YEAR_1, last_modified_year)
+        w = Weather.objects.retrieve_weather_object(city='San Jose', state='CA')
+        self.assertEqual(self.CURRENT_YEAR, w.last_modified.year)
+
+        w = Weather.objects.get(city='San Diego', state='CA')
+        last_modified_year = w.last_modified.year
+        self.assertEqual(self.LAST_UPDATED_YEAR_2, last_modified_year)
+        w = Weather.objects.retrieve_weather_object(city='San Diego', state='CA')
+        self.assertEqual(self.CURRENT_YEAR, w.last_modified.year)
+
+        w = Weather.objects.get(city='San Francisco', state='CA')
+        last_modified_year = w.last_modified.year
+        self.assertEqual(self.LAST_UPDATED_YEAR_3, last_modified_year)
+        w = Weather.objects.retrieve_weather_object(city='San Francisco', state='CA')
+        self.assertEqual(self.CURRENT_YEAR, w.last_modified.year)
+
+    def test_update_stale_data_bulk(self):
+        w0 = Weather.objects.get(city='San Francisco', state='CA')
+        w1 = Weather.objects.get(city='Merced', state='CA')
+        w2 = Weather.objects.get(city='Tracy', state='CA')
+
+        last_modified_year0 = w0.last_modified.year
+        last_modified_year1 = w1.last_modified.year
+        last_modified_year2 = w2.last_modified.year
+
+        self.assertEqual(self.LAST_UPDATED_YEAR_3, last_modified_year0)
+        self.assertEqual(self.LAST_UPDATED_YEAR_4, last_modified_year1)
+        self.assertEqual(self.LAST_UPDATED_YEAR_5, last_modified_year2)
+
+        locations = [('San Francisco', 'CA'), ('Merced', 'CA'), ('Tracy', 'CA')]
+        weathers = Weather.objects.retrieve_weather_objects(locations)
+        for weather in weathers:
+            self.assertEqual(self.CURRENT_YEAR, weather.last_modified.year)
+
+    def test_no_update_fresh_data_single(self):
+        w = Weather.objects.get(city='Azusa', state='CA')
+        weather = Weather.objects.retrieve_weather_object(city='Azusa', state='CA')
+        self.assertEqual(w.last_modified, weather.last_modified)
+
+    def test_no_update_fresh_data_bulk(self):
+        w0 = Weather.objects.get(city='Azusa', state='CA')
+        w1 = Weather.objects.get(city='Claremont', state='CA')
+        w2 = Weather.objects.get(city='Fresno', state='CA')
+
+        last_modifieds = [w0.last_modified, w1.last_modified, w2.last_modified]
+        locations = [('Azusa', 'CA'), ('Claremont', 'CA'), ('Fresno', 'CA')]
+        weathers = Weather.objects.retrieve_weather_objects(locations)
+        for i in range(0, len(weathers)):
+            self.assertEqual(last_modifieds[i], weathers[i].last_modified)
 
 
