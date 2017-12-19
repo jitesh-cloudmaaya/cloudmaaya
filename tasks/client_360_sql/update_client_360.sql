4ea5249dab5903def1eea027176d5bd381261fb9
Jay Salandanan <jsalandanan@raybeam.com>
2017-12-19 10:44:31 -0800

    decide on general strategy for updating the table

---
+++ tasks/client_360_sql/update_client_360.sql
@@ -0,0 +1,13 @@
+
+-- steps
+
+
+-- 1) create the order list view
+
+-- 2) set the session variable
+
+-- 3) call update on allume_client_360 table with correct logic
+-- UPDATE allume_client_360 
+-- SET (
+
+-- );
