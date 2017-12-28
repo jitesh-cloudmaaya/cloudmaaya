eb6091f1a38192847f5c236baf70f9ce0b2d69c6
Jay Salandanan <jsalandanan@raybeam.com>
2017-12-28 11:40:56 -0800

    initial commit and working with creating table mappings

--- tasks/product_feed_new.py
+++ tasks/product_feed_new.py
@@ -29,7 +29,6 @@ class ProductFeed(object):
         self._leave_temp_files = config_dict['leave_temp_files']
         self.etl_file_name = config_dict['etl_file_name']
 
-
         self._local_temp_dir_cleaned = self._local_temp_dir + '/cleaned'
         self._clean_data_method = config_dict['clean_data_method']
 
