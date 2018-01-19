52ece1e76b44e9b75bf3697300ca8657a025e679
Jay Salandanan <jsalandanan@raybeam.com>
2018-01-18 16:15:14 -0800

    base insert

--- tasks/look_metrics_sql/look_metrics.sql
+++ tasks/look_metrics_sql/look_metrics.sql
@@ -8,6 +8,28 @@ CREATE TABLE shopping_tool_lookmetrics_temp (
     total_item_sales numeric(10, 2) NULL,
     store_rank numeric(10, 2) NULL,
     allume_look_id integer NOT NULL);
+
+
+
+-- LookMetrics are based off the Look they are foreign key'd to
+-- Using the allume_looks table, I should construct some of these fields here
+
+INSERT INTO shopping_tool_lookmetrics_temp (
+    average_item_price,
+    total_look_price,
+    total_favorites,
+    total_item_sales,
+    store_rank,
+    allume_look_id)
+SELECT
+    0 as average_item_price,
+    0 as total_look_price,
+    0 as total_favorites,
+    0 as stotal_item_sales,
+    0 as store_rank,
+    al.id FROM allume_looks al;
+
+
 CREATE INDEX shopping_tool_lookmetrics_allume_look_id_d50ba475 ON shopping_tool_lookmetrics_temp (allume_look_id);
 
 DROP TABLE IF EXISTS shopping_tool_lookmetrics;
