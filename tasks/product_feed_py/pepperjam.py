564dc7e2066e1ce5e2366054398b85682c8e229c
Jay Salandanan <jsalandanan@raybeam.com>
2018-01-30 16:58:14 -0800

    fix error in exception handling of prices

--- tasks/product_feed_py/pepperjam.py
+++ tasks/product_feed_py/pepperjam.py
@@ -218,7 +218,7 @@ def get_data(local_temp_dir):
                         else:
                             record['current_price'] = retail_price
                     except:
-                        record['current_price']
+                        record['current_price'] = retail_price
 
                     # is_deleted logic
                     if 'modification' == 'D': # hardcoded false?
