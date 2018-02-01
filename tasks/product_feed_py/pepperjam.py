c89f842e74a7ab070acf8d61667c77bf44a81010
Jay Salandanan <jsalandanan@raybeam.com>
2018-02-01 10:08:31 -0800

    switch to api calls

--- tasks/product_feed_py/pepperjam.py
+++ tasks/product_feed_py/pepperjam.py
@@ -25,15 +25,16 @@ def get_merchants(status='joined'):
 
     ## Dev Only
     # # Test Merchants Data
-    print("Getting local test data")
-    json_data = open('tasks/product_feed_py/sample_data/pepperjam_merchant.json')
-    merchants = json.load(json_data)
-    json_data.close()
+    # print("Getting local test data")
+    # json_data = open('tasks/product_feed_py/sample_data/pepperjam_merchant.json')
+    # merchants = json.load(json_data)
+    # json_data.close()
 
     # Get Merchants
-    # print 'Getting merchants using API call'
-    # json_data = open_w_timeout_retry(pepper_jam_api_merchant_url, 4, 60, 3, 2)
-    # merchants = json.load(json_data)
+    ## Prod & Staging Only
+    print 'Getting merchants using API call'
+    json_data = open_w_timeout_retry(pepper_jam_api_merchant_url, 4, 60, 3, 2)
+    merchants = json.load(json_data)
     
     # Create some variables to count process metrics
     new_merchants = 0
@@ -94,19 +95,19 @@ def get_data(local_temp_dir, cleaned_fieldnames):
 
             # commenting out because API only has X amount of access allowed in a day
             ## Prod & Staging Only
-            # print 'Getting data using the API calls'
-            # print("Getting Data")
+            print 'Getting data using the API calls'
+            print("Getting Data")
 
-            # print(pepper_jam_api_product_url)
-            # json_data = open_w_timeout_retry(pepper_jam_api_product_url, 4, 60, 3, 2)
-            # product_feed = json.load(json_data)
+            print(pepper_jam_api_product_url)
+            json_data = open_w_timeout_retry(pepper_jam_api_product_url, 4, 60, 3, 2)
+            product_feed = json.load(json_data)
 
             ## Dev Only
-            print("Getting Data")
-            print(pepper_jam_api_product_url)
-            json_data = open('tasks/product_feed_py/sample_data/pepperjam_product.json')
-            product_feed = json.load(json_data)
-            json_data.close()
+            # print("Getting Data")
+            # print(pepper_jam_api_product_url)
+            # json_data = open('tasks/product_feed_py/sample_data/pepperjam_product.json')
+            # product_feed = json.load(json_data)
+            # json_data.close()
 
             if 'next' in product_feed['meta']['pagination']:
                 pepper_jam_api_product_url = product_feed['meta']['pagination']['next']['href']
