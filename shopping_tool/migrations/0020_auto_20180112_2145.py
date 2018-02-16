dd3469be576529eb474b85d66480d9bb360cc211
Jay Salandanan <jsalandanan@raybeam.com>
2018-02-16 09:54:30 -0800

    also update the test migrations

--- shopping_tool/migrations/0020_auto_20180112_2145.py
+++ shopping_tool/migrations/0020_auto_20180112_2145.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
             migrations.AddField(
                 model_name='allumelookproducts',
                 name='cropped_dimensions',
-                field=models.CharField(blank=True, max_length=200, null=True),
+                field=models.CharField(blank=True, max_length=2000, null=True),
             ),
             migrations.AddField(
                 model_name='allumelookproducts',
