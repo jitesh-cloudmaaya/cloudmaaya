2b9b589a319d50a28a52e85ac620514051e89cfa
Jay Salandanan <jsalandanan@raybeam.com>
2018-03-08 14:31:48 -0800

    add API route to get product by id and return json payload with structure of allume product

--- product_api/urls.py
+++ product_api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^basic_search$', views.basic_search, name='basic_search'),
     url(r'^facets$', views.facets, name='facets'),
     url(r'^get_product/(?P<product_id>[0-9]+)/$', views.get_product, name='get_product'),
+    url(r'^get_allume_product/(?P<product_id>[0-9]+)/$', views.get_allume_product, name='get_allume_product')
 ]
--- product_api/views.py
+++ product_api/views.py
@@ -158,7 +158,101 @@ def get_product(self, product_id):
 
     return Response(context) 
 
+@api_view(['GET'])
+@permission_classes((AllowAny, ))
+def get_allume_product(self, product_id):
+    """
 
+    get:
+        Convert product data to allume product data by id
+        URL: /product_api/super_get_product/1570
+    """
+    product = Product.objects.get(id = product_id)
+    p_name = product.product_name
+
+    # using the product we are looking for
+    product_merchant_id = product.merchant_id
+    # retrieve the merchant from the pam table
+    merchant = Merchant.objects.get(external_merchant_id = product_merchant_id)
+    # and grab the id used in the internal db representation
+    merchant_id = merchant.id
+
+    s = Search(index="products")
+
+    s = s.query("match_phrase", product_name=product.product_name)[0:100]
+    s = s.filter("match_phrase", merchant_name=product.merchant_name)
+
+    results = s.execute()
+
+    results_dict = results.to_dict()
+    # grab and add _source field dict to add field for API call
+    for hit in results_dict['hits']['hits']:
+        hit['_source']['product_api_merchant'] = merchant_id
+
+    total_count = s.count()
+    page = 1
+    facets_dict = {}
+    context = format_results(results_dict, total_count, page, 100, self, 'products', p_name, facets_dict)
+
+
+    results = context
+
+
+    payload = {'sites': {}}
+    tmp = {'color_names': [], 'color_objects': {}}
+    matching_object = ''
+
+    data = results['data']
+    # loop through results to set up content for the payload
+    for i in range(0, len(data)):
+        product = data[i]['_source']
+        if str(product['id']) == str(product_id) or str(product['product_id']) == str(product_id):
+            matching_object = product
+        # create color object for payload
+        clr = product['merchant_color'].lower()
+        if clr not in tmp['color_names']:
+            tmp['color_names'].append(clr)
+            tmp['color_objects'][clr] = {'sizes': [], 'size_data': {}}
+
+        all_sizes = product['size'].split(',')
+        for i in range(0, len(all_sizes)):
+            size = all_sizes[i]
+            if size not in tmp['color_objects'][clr]['sizes']:
+                tmp['color_objects'][clr]['sizes'].append(size)
+                size_data = {'image': product['product_image_url'], 'price': product['current_price'], 'text': size, 'value': size} # change formatting?
+                tmp['color_objects'][clr]['size_data'][size] = size_data
+
+    # create payload object
+    merchant_node = str(matching_object['product_api_merchant'])
+    product_node = str(product_id)
+    payload['sites'][merchant_node] = {}
+    payload['sites'][merchant_node]['add_to_cart'] = {}
+    payload['sites'][merchant_node]['add_to_cart'][product_node] = {}
+    payload['sites'][merchant_node]['add_to_cart'][product_node]['title'] = matching_object['product_name']
+    payload['sites'][merchant_node]['add_to_cart'][product_node]['brand'] = matching_object['brand']
+    payload['sites'][merchant_node]['add_to_cart'][product_node]['price'] = matching_object['current_price']
+    payload['sites'][merchant_node]['add_to_cart'][product_node]['original_price'] = matching_object['retail_price']
+    payload['sites'][merchant_node]['add_to_cart'][product_node]['image'] = matching_object['product_image_url']
+    payload['sites'][merchant_node]['add_to_cart'][product_node]['description'] = matching_object['long_product_description']
+    payload['sites'][merchant_node]['add_to_cart'][product_node]['required_field_names'] = ["color", "size", "quantity"]
+    payload['sites'][merchant_node]['add_to_cart'][product_node]['required_field_values'] = {}
+    payload['sites'][merchant_node]['add_to_cart'][product_node]['required_field_values']['color'] = []
+    payload['sites'][merchant_node]['add_to_cart'][product_node]['url'] = matching_object['product_url']
+    payload['sites'][merchant_node]['add_to_cart'][product_node]['status'] = "done"
+    payload['sites'][merchant_node]['add_to_cart'][product_node]['original_url'] = matching_object['raw_product_url']
+
+    # create the colors array object
+    for i in range(0, len(tmp['color_names'])):
+        color = tmp['color_names'][i]
+        obj = {'dep': {'size': []}, 'price': matching_object['current_price'], 'text': color, 'value': color}
+        for j in range(0, len(tmp['color_objects'][color]['sizes'])):
+            size_name = tmp['color_objects'][color]['sizes'][j]
+            size = tmp['color_objects'][color]['size_data'][size_name]
+            size['dep'] = {}
+            obj['dep']['size'].append(size)
+        payload['sites'][merchant_node]['add_to_cart'][product_node]['required_field_values']['color'].append(obj)
+
+    return Response(payload)
 
 def format_results(results, total_count, page, num_per_page, request, label, text_query, facets_dict):
     response = collections.OrderedDict()
