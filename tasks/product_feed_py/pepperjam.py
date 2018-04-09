b9b57f93644e61c40c20b3181209df038b1ce171
Jay Salandanan <jsalandanan@raybeam.com>
2018-04-09 16:20:58 -0700

    comment back in set_deleted pepperjam

--- tasks/product_feed_py/pepperjam.py
+++ tasks/product_feed_py/pepperjam.py
@@ -339,7 +339,7 @@ def get_data(local_temp_dir, cleaned_fieldnames, dev=False):
 
     # call update_pepperjam here?
     print('Updating non-upserted records')
-    # product_feed_helpers.set_deleted_network_products('PepperJam')
+    product_feed_helpers.set_deleted_network_products('PepperJam')
 
 def generate_product_id_pepperjam(SKU, merchant_id):
     """
