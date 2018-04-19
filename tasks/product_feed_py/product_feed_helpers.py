f709a279b74f71d7f4ae77b39c0814e30b520163
Jay Salandanan <jsalandanan@raybeam.com>
2018-04-18 17:18:27 -0700

    initial setup for email service

--- tasks/product_feed_py/product_feed_helpers.py
+++ tasks/product_feed_py/product_feed_helpers.py
@@ -510,3 +510,34 @@ def normalize_data(text, form='NFD'):
     text = text.encode('ascii', 'ignore')
     text = text.decode('utf-8')
     return text
+
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from catalogue_service.settings import ALERT_EMAIL_ADDRESS, ALERT_EMAIL_PASSWORD
+def send_email():
+    """
+    Helper to support sending email as part of failure tracking of a job
+    """
+
+
+    fromaddr = ALERT_EMAIL_ADDRESS
+    toaddr = ALERT_EMAIL_ADDRESS # this part is a placeholder
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = 'Placeholder (TBD)'
+
+    body = 'This is a test message.'
+    msg.attach(MIMEText(body, 'plain'))
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(fromaddr, ALERT_EMAIL_PASSWORD)
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
+
+
+
+
