fa90bd5bdfd6be48a6c63eab1e0ff353725fd5c4
Wes Duenow <wjduenow@raybeam.com>
2018-05-08 17:17:19 -0700

    getting new relic into celery and celerybeat

--- docker_config_files/docker-entrypoint-celery-beat.sh
+++ docker_config_files/docker-entrypoint-celery-beat.sh
@@ -7,4 +7,8 @@ touch /srv/log/logfile
 #sudo service celeryd start
 export C_FORCE_ROOT=true
 #tail -n 0 -f /srv/logs/*.log
-celery -A catalogue_service beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
+
+NEW_RELIC_CONFIG_FILE=newrelic.ini
+export NEW_RELIC_CONFIG_FILE
+
+newrelic-admin run-program celery -A catalogue_service beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
