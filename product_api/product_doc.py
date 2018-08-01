e11645c29c42e60c70eb376b949d25a866189322
pnguesseu <pamelanguesseu@yahoo.fr>
2018-08-01 10:17:36 -0700

    AC-184 Migrating elasticsearch_dsl from 5.0.0 to 6.0.0 as thats where the diversifier sampler class was added

--- product_api/product_doc.py
+++ product_api/product_doc.py
@@ -134,7 +134,7 @@ class EProductSearch(FacetedSearch):
         Add aggregations representing the facets selected, including potential
         filters.
         """
-        search.aggs.bucket('unbiased_retailer_agg', 'diversified_sampler', field='merchant_name', shard_size=10)
+        res = search.aggs.bucket('unbiased_retailer_agg', 'diversified_sampler', field='merchant_name', shard_size=10)
         for f, facet in iteritems(self.facets):
             agg = facet.get_aggregation()
             agg_filter = Q('match_all')
@@ -142,7 +142,7 @@ class EProductSearch(FacetedSearch):
                 if f == field:
                     continue
                 agg_filter &= filter
-            search.aggs.bucket(
+            res.bucket(
                 '_filter_' + f,
                 'filter',
                 filter=agg_filter
