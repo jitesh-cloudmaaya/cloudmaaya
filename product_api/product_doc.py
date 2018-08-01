87c2b109226398480a0d95ea5abeff819c39f213
pnguesseu <pamelanguesseu@yahoo.fr>
2018-08-01 07:44:21 -0700

    AC-184 adding diversifier sampler on the retailer field

--- product_api/product_doc.py
+++ product_api/product_doc.py
@@ -134,6 +134,7 @@ class EProductSearch(FacetedSearch):
         Add aggregations representing the facets selected, including potential
         filters.
         """
+        search.aggs.bucket('unbiased_retailer_agg', 'diversified_sampler', field='merchant_name', shard_size=10)
         for f, facet in iteritems(self.facets):
             agg = facet.get_aggregation()
             agg_filter = Q('match_all')
