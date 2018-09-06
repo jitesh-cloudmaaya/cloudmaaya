3dddb8dbbc5e7e32c39b95c460fdbd0c7c2a0c86
Yicong Gong <yiconggong@yicongs-mbp.lan>
2018-09-05 17:11:21 -0700

    AC-207 Prevent product from turned off merchant being copied - fixed decorator.py and the name of flag

--- shopping_tool/decorators.py
+++ shopping_tool/decorators.py
@@ -7,8 +7,8 @@ from catalogue_service.settings_local import AUTH_LOGIN_URL, AUTH_EMAIL_KEY, AUT
 # https://simpleisbetterthancomplex.com/2015/12/07/working-with-django-view-decorators.html
 def check_login(function):
     def wrap(request, *args, **kwargs):
-        if True:
-            user_email = 'pamela@allume.co' #request.COOKIES[AUTH_EMAIL_KEY]
+        if request.COOKIES.get(AUTH_EMAIL_KEY, False):
+            user_email = request.COOKIES[AUTH_EMAIL_KEY]
             user = WpUsers.objects.get(user_email = user_email)
             request.user = user
             return function(request, *args, **kwargs)
